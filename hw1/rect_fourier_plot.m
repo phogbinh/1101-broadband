2f3d2e46b71c3d640baac2fb0d32ef1499f70545
Tran Phong Binh (Bill) <32406100+phogbinh@users.noreply.github.com>
2021-10-28 13:40:06 +0800

    [master] Added a space.

--- hw1/rect_fourier_plot.m
+++ hw1/rect_fourier_plot.m
@@ -5,7 +5,7 @@ Y1_WIDTH = 1;
 Y2_WIDTH = 0.5;
 FREQUENCY_INTERVAL = POINTS_N / TIME_INTERVAL;
 %% create functions
-t = (-POINTS_N / 2) * TIME_INTERVAL:TIME_INTERVAL:(POINTS_N / 2- 1) * TIME_INTERVAL;
+t = (-POINTS_N / 2) * TIME_INTERVAL:TIME_INTERVAL:(POINTS_N / 2 - 1) * TIME_INTERVAL;
 x1 = rectpuls(t, Y1_WIDTH);
 x2 = rectpuls(t, Y2_WIDTH);
 y1 = fft(x1);
